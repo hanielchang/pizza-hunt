e2e14ba793d87b5c19181927f23c3d9208bd87ba
hanielchang <haniellloyd@gmail.com>
2021-10-18 23:37:16 -0700

    added reply functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,62 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
+);
+
+// We update the CommentSchema to have the replies field populated with an array of data that adheres to the ReplySchema definition. 
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of replies on retrieval
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
--- models/Pizza.js
+++ models/Pizza.js
@@ -40,7 +40,7 @@ const PizzaSchema = new Schema(
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function () {
-  return this.comments.length;
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
@@ -53,3 +53,9 @@ module.exports = Pizza;
 // They're normally computed values that get evaluated when you try to access their properties.
 // Virtuals allow us to add more information to a database response so that we don't have 
 // to add in the information manually with a helper before responding to the API request.
+
+// ----------------line 43----------------
+// In its basic form, .reduce() takes two parameters, an accumulator and a currentValue. 
+// Here, the accumulator is total, and the currentValue is comment. As .reduce() walks through 
+// the array, it passes the accumulating total and the current value of comment into the function, 
+// with the return of the function revising the total for the next iteration through the array.
